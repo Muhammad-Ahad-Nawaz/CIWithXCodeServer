5422068261ef8a24862afd7df237343a5e8a70c2
Ahad's iMac <ahadnawaz@ahadss-imac.local>
2018-10-08 17:14:30 +0400

    Initial Commit

---
+++ CIWithXCodeServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIWithXCodeServer
+//
+//  Created by Ahad's iMac on 10/8/18.
+//  Copyright © 2018 Ahad's iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
